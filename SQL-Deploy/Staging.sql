c5f80bc094dc94c344304b220305fb851df5b0b7
willem vermeulen <willem.j.vermeulen@gmail.com>
2018-05-16 16:19:09 +0100

    tt

--- SQL-Deploy/Staging.sql
+++ SQL-Deploy/Staging.sql
@@ -1,3 +1,2 @@
-ALTER TABLE [dbo].[Table_1] ADD [test] [nchar](10) NULL
-ALTER TABLE [dbo].[Table_1] ADD [another] [nchar](10) NULL
-'sqlcmd -S DESKTOP-NS5M003 -U sa -P Annette4me -d Staging -i E:\REPO\jenkins-test\jenkinstest\SQL-Deploy\Staging.sql
+ALTER TABLE [dbo].[Table_1] ADD [test1] [nchar](10) NULL
+ALTER TABLE [dbo].[Table_1] ADD [another1] [nchar](10) NULL
